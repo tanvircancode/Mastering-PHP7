1487e6f0518e581ea61c8cd52db8b4c86c942750
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2020-08-21 16:55:04 +0600

    Add files via upload

---
+++ প্রজেক্ট ডেটা স্ক্র‍্যাম্বলার/scramblerf.php
@@ -0,0 +1,46 @@
+<?php
+
+function displayKey($key)
+{
+    // global $task;
+    // if('key' == $task)
+    // {
+    printf("Value = %s", $key);
+    //}
+}
+
+function scrambleData($originalData, $key)
+{
+    $originalKey = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    $data = '';
+    $length = strlen($originalData);
+
+    for ($i = 0; $i < $length; $i++) {
+        $currentChar = $originalData[$i];
+        $position = strpos($originalKey, $currentChar);
+        if ($position !== false) {
+            $data .= $key[$position];
+        } else {
+            $data .= $currentChar;
+        }
+    }
+    return $data;
+}
+
+function decodeData($originalData, $key)
+{
+    $originalKey = 'abcdefghijklmnopqrstuvwxyz1234567890';
+    $data = '';
+    $length = strlen($originalData);
+
+    for ($i = 0; $i < $length; $i++) {
+        $currentChar = $originalData[$i];
+        $position = strpos($key, $currentChar);
+        if ($position !== false) {
+            $data .= $originalKey[$position];
+        } else {
+            $data .= $currentChar;
+        }
+    }
+    return $data;
+}
